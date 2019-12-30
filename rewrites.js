02bb5d754ccfdf5e40a2febddc78553190871833
Artem Telesh <decarea@yahoo.com>
2019-12-29 22:33:55 -0500

    converted to fetch: to work from URL Rewrites page

--- rewrites.js
+++ rewrites.js
@@ -1,18 +1,30 @@
-let form = document.getElementById('edit_form');
+const button = document.getElementById('add');
+// find the link to add new URL rewrite page
+const link = button.attributes.onclick.nodeValue.slice(13, -2);
 
-if (form.request_path && form.target_path && form.redirect_type) {
-  form.request_path.value = '1';
-  form.target_path.value = '2';
-  const options = Array.from(form.redirect_type.options).map(x => x.value);
-  const index = options.indexOf("301");
-  if (index != -1) {
-    form.redirect_type.selectedIndex = index;
+fetch(link)
+.then(response => response.text())
+.then(data => {
+  let parser = new DOMParser();
+  let page = parser.parseFromString(data, 'text/html');
+  let form = page.getElementById('edit_form');
+  
+  if (form.request_path && form.target_path && form.redirect_type) {
+    form.request_path.value = '1';
+    form.target_path.value = '2';
+    const options = Array.from(form.redirect_type.options).map(x => x.value);
+    const index = options.indexOf("301");
+    if (index != -1) {
+      form.redirect_type.selectedIndex = index;
+      console.log('all good! found all required fields');
+    }
+    else {
+      console.error('301 redirect option was not found');
+    }
+    // form.submit();
   }
   else {
-    console.error('301 redirect option was not found')
+    console.error('this is not a 301 redirect form. missing required inputs');
   }
-  form.submit();
-}
-else {
-  console.error('this is not a 301 redirect form. missing required inputs');
-}
+})
+
